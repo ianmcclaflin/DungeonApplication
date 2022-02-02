4558b82250b133b45bd566f95fbdfea5d82c357c
Ian McClaflin <ian.mcclaflin@outlook.com>
2022-02-02 05:36:02 -0600

    Initial Commit.

---
+++ DungeonApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dungeon Application");
+        }
+    }
+}
